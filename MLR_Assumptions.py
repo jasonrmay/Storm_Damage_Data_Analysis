d3e78c80dcfa9e0300a913fb18347e56cc417871
jasonrmay <jasonrmay04@utexas.edu>
2025-12-14 14:09:03 -0600

    Proper files types.

---
+++ MLR_Assumptions.py
@@ -0,0 +1,6 @@
+class MLR_Assumption_Checker:
+    """
+    Assuming random and independent sample, check residuals for normality, linearity, and equal variance.
+    """
+    def __init__(self):
+        pass
